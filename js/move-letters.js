52dc4c0836d3485a9020d40a810720d48c9b3f06
Aysur <78086194+aysur12@users.noreply.github.com>
2025-03-17 21:41:40 +0700

    final

---
+++ js/move-letters.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', () => {
+  class CommentCards {
+    constructor(container) {
+      this.container = container;
+      this.cards = Array.from(container.querySelectorAll('.card'));
+      this.current =
+        this.cards.find((card) => card.classList.contains('card--current')) || this.cards[this.cards.length - 1];
+      this.init();
+    }
+
+    init() {
+      this.cards.forEach((card) => {
+        card.addEventListener('click', (e) => this.handleClick(e, card));
+        const nextBtn = card.querySelector('.next-btn');
+        if (nextBtn) {
+          nextBtn.addEventListener('click', (e) => this.handleNextClick(e, card));
+        }
+      });
+
+      if (!this.current) {
+        this.current = this.cards[this.cards.length - 1];
+        this.cards[0].click();
+      }
+      this.container.classList.add('cards--active');
+    }
+
+    handleClick(event, clickedCard) {
+      if (this.current !== clickedCard) {
+        this.updateClasses(clickedCard);
+      }
+    }
+
+    handleNextClick(event, clickedCard) {
+      event.stopPropagation();
+
+      if (clickedCard.classList.contains("card-2")) {
+        const dateInput = clickedCard.querySelector('.input');
+        if (!dateInput || !dateInput.value) {
+          alert("Пожалуйста, выберите дату)");
+          return;
+        }
+      }
+
+      const nextIndex = (this.cards.indexOf(clickedCard) + 1) % this.cards.length;
+      this.updateClasses(this.cards[nextIndex]);
+    }
+
+    updateClasses(newCurrent) {
+      this.cards.forEach((card) => card.classList.remove('card--current', 'card--out', 'card--next'));
+      this.current.classList.add('card--out');
+      this.current = newCurrent;
+      this.current.classList.add('card--current');
+
+      const nextIndex = (this.cards.indexOf(this.current) + 1) % this.cards.length;
+      this.cards[nextIndex].classList.add('card--next');
+    }
+  }
+
+  document.querySelectorAll('.cards').forEach((container) => new CommentCards(container));
+});
--- js/script.js
+++ js/script.js
@@ -1,14 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
   const yesButton = document.querySelector('.yes');
+  const noButton = document.querySelector('.no');
 
   const submitData = (e) => {
+    noButton.classList.add('hidden');
+
     fetch('https://dating-63e93-default-rtdb.europe-west1.firebasedatabase.app/agreements.json', {
       method: 'POST',
       body: JSON.stringify({
         agreement: 'yes',
       }),
-    })
+    });
   };
 
   yesButton.addEventListener('click', submitData);
+
+  const form = document.querySelector('.form');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Получаем значение из поля ввода
+    const dateValue = document.getElementById('date').value;
+
+    // Создаем объект с данными для отправки
+    const data = {
+      date: dateValue,
+    };
+
+    // Отправляем данные на сервер с помощью fetch
+    fetch('https://dating-63e93-default-rtdb.europe-west1.firebasedatabase.app/date.json', {
+      method: 'POST', // Используем метод POST для отправки данных
+      headers: {
+        'Content-Type': 'application/json', // Указываем, что данные в формате JSON
+      },
+      body: JSON.stringify(data), // Преобразуем объект в JSON строку
+    })
+      .then((response) => response.json()) // Парсим ответ от сервера в формате JSON
+      .then((data) => {
+        console.log('Success:', data); // Логируем успех
+        // Здесь можно обработать успешный ответ от сервера, например, показать сообщение
+      })
+      .catch((error) => {
+        console.error('Error:', error); // Логируем ошибку в случае неудачи
+      });
+  });
 });
